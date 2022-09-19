0e02eb23d307275f41e6b8a76e0e6623204b6f99
haneefa <mohamed.haneefa@agiratech.com>
2022-09-19 17:25:50 +0530

    absoulute path added

--- src/api.js
+++ src/api.js
@@ -148,11 +148,7 @@ router.get("/file/upload", async (req, res) => {
   try {
     const s3Stream = await s3.getObject(bucketParams).createReadStream();
 
-    console.log("s3Stream :", s3Stream);
-
     const stream = await fs.createWriteStream(csvFilePath);
-
-    console.log("Stream :", stream);
 
     s3Stream.pipe(stream).on("finish", () => {
       const results = [];
